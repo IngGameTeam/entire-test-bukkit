98770d1d6ab0f39d28976e177001adea839c5446
Bruce0203 <56539682+Bruce0203@users.noreply.github.com>
2022-07-17 13:15:32 +0000

    commit

---
+++ .github/workflows/setup-bukkit.sh
@@ -0,0 +1,40 @@
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+
+echo "setup bukkit started"
+
+# bash script args
+echo $(pwd)
+parse_yaml setup-bukkit.yml
+
+BUKKIT_FILE_NAME="server.jar"
+# download bukkit
+wget -q -O $BUKKIT_FILE_NAME https://api.purpurmc.org/v2/purpur/$version/latest/download
+
+# set eula.txt to eula=true
+rm -rf eula.txt
+touch eula.txt
+echo 'eula=true' >> eula.txt 
+
+# generate run.sh shell file 
+rm -rf run.sh
+touch run.sh
+echo "java -Xms$ram -Xmx$ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $BUKKIT_FILE_NAME nogui" >> run.sh
+chmod +x ./run.sh
+
+echo "setup bukkit finished"
