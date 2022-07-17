9a59f337aa25e553d8f9cbeec3a77c9d40e76434
Bruce0203 <56539682+Bruce0203@users.noreply.github.com>
2022-07-17 12:43:38 +0000

    commit

--- .github/workflows/setup-bukkit.sh
+++ .github/workflows/setup-bukkit.sh
@@ -4,8 +4,9 @@ echo "setup bukkit started"
 MC_VERSION=$1
 RAM=$2
 
+BUKKIT_FILE_NAME="server.jar"
 # download bukkit
-wget -q -O purpur.jar https://api.purpurmc.org/v2/purpur/1.19/latest/download
+wget -q -O $BUKKIT_FILE_NAME https://api.purpurmc.org/v2/purpur/1.19/latest/download
 
 # set eula.txt to eula=true
 rm -rf eula.txt
@@ -15,7 +16,7 @@ echo 'eula=true' >> eula.txt
 # generate run.sh shell file 
 rm -rf run.sh
 touch run.sh
-echo 'java -Xms${{ env.ram }} -Xmx${{ env.ram }} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar purpur.jar nogui' >> run.sh
+echo 'java -Xms$RAM -Xmx$RAM -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $BUKKIT_FILE_NAME nogui' >> run.sh
 chmod +x ./run.sh
 
 echo "setup bukkit finished"
