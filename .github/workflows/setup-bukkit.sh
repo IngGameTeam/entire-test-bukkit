8f32ab9aa4eef8fbf4840df31cb75a4d81c2faca
Bruce0203 <56539682+Bruce0203@users.noreply.github.com>
2022-07-17 12:37:21 +0000

    corrected to ignore warning

--- .github/workflows/setup-bukkit.sh
+++ .github/workflows/setup-bukkit.sh
@@ -1,4 +1,4 @@
-echo "install bukkit started"
+echo "setup bukkit started"
 
 # bash script args
 MC_VERSION=$1
@@ -19,7 +19,7 @@ cd $BUKKIT_DIR
 wget -O purpur.jar https://api.purpurmc.org/v2/purpur/1.19/latest/download
 
 # set eula.txt to eula=true
-rm -r eula.txt
+rm -rf eula.txt
 touch eula.txt
 echo 'eula=true' >> eula.txt 
 
@@ -29,4 +29,4 @@ touch run.sh
 echo 'java -Xms${{ env.ram }} -Xmx${{ env.ram }} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar purpur.jar nogui' >> run.sh
 chmod +x ./run.sh
 
-echo "install bukkit finished"
+echo "setup bukkit finished"
